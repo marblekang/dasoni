9062d1805e91b8c0b8f34489c05597fa49294f99
강병민 <kang@gangbyeongmin-ui-noteubug.local>
2024-12-14 03:37:10 +0900

    feat

--- src/app/(authorized)/(with-search-bar)/main/[tabType]/page.tsx
+++ src/app/(authorized)/(with-search-bar)/main/[tabType]/page.tsx
@@ -1,16 +1,20 @@
 import Nav from "@/components/common/nav/Nav";
 import DiaryList from "@/components/diary/DiaryList";
 import UserList from "@/components/user/UserList";
+import { Metadata } from "next";
 import React from "react";
-
-const Page = ({
-  params,
-  searchParams,
-}: {
+interface PageProps {
   params: { tabType: "user" | "diary" };
-  searchParams: { keyword: string };
-}) => {
-  const tabType = params?.tabType;
+  searchParams: { keyword?: string };
+}
+
+export const metadata: Metadata = {
+  title: "Dynamic Tab Page",
+};
+
+const Page = ({ params, searchParams }: PageProps) => {
+  const tabType = params?.tabType ?? "user";
+
   return (
     <div className="w-full bg-[#FFFCFD] relative">
       <Nav tabType={tabType} />
@@ -18,7 +22,7 @@ const Page = ({
         {tabType === "diary" ? (
           <DiaryList keyword={searchParams?.keyword ?? ""} />
         ) : (
-          <UserList keyword={searchParams.keyword} />
+          <UserList keyword={searchParams?.keyword ?? ""} />
         )}
       </div>
     </div>
