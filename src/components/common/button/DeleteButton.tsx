6af0c5f60109dc25dff1f8d74e1117c4291802f6
강병민 <kang@gangbyeongmin-ui-noteubug.local>
2024-12-14 23:27:40 +0900

    fix: localStorage에 접근하는 client 컴포넌트에서 useEffect 사용

--- src/components/common/button/DeleteButton.tsx
+++ src/components/common/button/DeleteButton.tsx
@@ -1,17 +1,21 @@
 "use client";
 import { getAccessTokenByClient } from "@/utils/getCookieByClient";
 import { deleteRequest } from "@/utils/requets";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const DeleteButton = ({ children }: { children: string }) => {
-  const token = getAccessTokenByClient();
+  const [accessToken, setAccessToken] = useState<string>("");
+  useEffect(() => {
+    const token = getAccessTokenByClient();
+    setAccessToken(token);
+  }, [accessToken]);
   return (
     <div
       className="py-2 cursor-pointer"
       onClick={() =>
         deleteRequest({
           url: "/diary",
-          headers: { Authorization: token ?? "" },
+          headers: { Authorization: accessToken || "" },
         })
           .then(() => {
             alert("편지가 전부 삭제되었습니다.");
